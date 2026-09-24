0d288f7868ae44c28da227424a3f9c1fcc282b65
Hashib2001012 <2001012@iot.bdu.ac.bd>
2026-09-25 02:50:04 +0600

    Updated achievements and certificates gallery

--- assets/data.js
+++ assets/data.js
@@ -14,7 +14,6 @@ title:
 
 description:
 "1st Runner-Up | TeleVerse 1.0 | ETE Department, CUET"
-
 },
 
 
@@ -26,55 +25,24 @@ title:
 "🏆 Best Paper Award",
 
 description:
-"7th IEEE International Conference on Sustainable Technologies for Industry 5.0 | Track 02 | 2025"
-
-},
-
-
-
-
-{
-image:"ignition.jpg",
-
-title:
-"🏆 Project Showcasing",
-
-description:
-"Ignition 2026 National Mechanical Festival | Department of Mechanical Engineering, KUET"
-
-},
-
-
-
-
-{
-image:"Robofusion.jpg",
-
-title:
-"🏆 Executive Member",
-
-description:
-"WALTON Presents ROBOFUSION 1.0 | UFTB Robotics Club"
-
+"7th International Conference on Sustainable Technologies for Industry 5.0 | Track 02 | 2025"
 },
 
 
 
 {
-image:"iotrix.jpg",
+image:"esp.JPG",
 
 title:
-"🏆 Innovation Achievement",
+"🏆 ESPectrum 1.0 Project Showcasing",
 
 description:
-"IoT and Robotics Innovation Competition"
-
+"2nd Runner-Up | RUET IoT Club"
 }
 
 
 
 ];
-
 
 
 
@@ -92,20 +60,6 @@ const certificates = [
 
 
 {
-image:"ai-ml.jpg",
-
-title:
-"Artificial Intelligence and Machine Learning Bootcamp",
-
-description:
-"EATL Innovation Hub | 2025"
-
-},
-
-
-
-
-{
 image:"pcb.jpg",
 
 title:
@@ -113,7 +67,19 @@ title:
 
 description:
 "UFTB Career Center | 2025"
+},
 
+
+
+
+{
+image:"ai-ml.jpg",
+
+title:
+"Artificial Intelligence and Machine Learning Bootcamp",
+
+description:
+"EATL Innovation Hub | 2025"
 },
 
 
@@ -127,7 +93,6 @@ title:
 
 description:
 "School of Computer Applications, KIIT | 2025"
-
 },
 
 
@@ -137,13 +102,50 @@ description:
 image:"Robofusion.jpg",
 
 title:
-"Robofusion 1.0",
+"ROBOFUSION 1.0",
 
 description:
-"Executive Member | UFTB Robotics Club"
+"UFTB Robotics Club"
+},
 
+
+
+
+{
+image:"ignition.jpg",
+
+title:
+"Ignition 2026 National Mechanical Festival",
+
+description:
+"Project Showcase Participation"
+},
+
+
+
+
+{
+image:"iotrix.jpg",
+
+title:
+"IoTrix National IoT Hackathon",
+
+description:
+"TeleVerse 1.0 | CUET"
+},
+
+
+
+
+{
+image:"best-paper.jpg",
+
+title:
+"Best Paper Recognition",
+
+description:
+"IEEE Conference Achievement"
 }
-
 
 
 
